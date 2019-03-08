de4f6542335b7eb7a4963b7e467cf841641e76cd
Cristhian Urrego Salazar <infoafuro@gmail.com>
2019-03-08 17:23:15 -0500

    avance threads

--- 05 calculadora_matrices/menu.py
+++ 05 calculadora_matrices/menu.py
@@ -1,4 +1,4 @@
-import os, time
+import os, time, threading
 
 clear = lambda: os.system('cls')
 
@@ -90,25 +90,25 @@ def one_matrix(matrix_1):
             option = int(option)
             while switch(option):
                 if case(1):
-                    determinante()
+                    print("\n [DETERMINANTE]: " + str(determinante(matrix_1)))
                     break
                 if case(2):
-                    transpuesta()
+                    transpuesta(matrix_1)
                     break
                 if case(3):
-                    inversa()
+                    inversa(matrix_1)
                     break
                 if case(4):
-                    multiplicar()
+                    multiplicar(matrix_1)
                     break
                 if case(5):
-                    elevada()
+                    elevada(matrix_1)
                     break
                 if case(6):
-                    matriz_simetrica()
+                    matriz_simetrica(matrix_1)
                     break
                 if case(7):
-                    matriz_identidad()
+                    matriz_identidad(matrix_1)
                     break
             break
         except ValueError:
@@ -140,45 +140,107 @@ def two_matrix(matrix_1, matrix_2):
             print("\n\n¡Opción invalida!")
             time.sleep(.500)
 
-def determinante():
+def determinante(matrix_1):
 
-    pass
-def transpuesta():
-    pass
-def inversa():
-    pass
-def multiplicar(m):
-    threads = list()
+    def lado_a(matrix_1, x, y):
+        resultado_temp = 1
+        for pos in range(3):
+            if x > 2:
+                x = 0
+            if y > 2:
+                y = 0
+            resultado_temp = resultado_temp * matrix_1[x][y]
+            x = x+1
+            y = y+1
+        return resultado_temp
 
-    num = input("Multiplicar por: ")
-    for x in range(x_size):
-        for y in range(y_size):
-            # matrix = threading.Thread(target=)
-            print('['+str(matrix[x][y]), end=']\t')
+    def lado_b(matrix_1, x, y):
+        resultado_temp = 1
+        determinante = 0
+        for pos in range(3):
+            if x > 2:
+                x = 0
+            if y < 0:
+                y = 2
+            resultado_temp = resultado_temp * matrix_1[x][y]
+            x = x+1
+            y = y-1
+        return resultado_temp
 
+    temp_1 = threading.Thread(target=lado_a, args=(matrix_1, 0, 0))
+    temp_2 = threading.Thread(target=lado_a, args=(matrix_1, 0, 1))
+    temp_3 = threading.Thread(target=lado_a, args=(matrix_1, 0, 2))
+    temp_4 = threading.Thread(target=lado_b, args=(matrix_1, 0, 2))
+    temp_5 = threading.Thread(target=lado_b, args=(matrix_1, 0, 0))
+    temp_6 = threading.Thread(target=lado_b, args=(matrix_1, 0, 1))
 
-    threads = list()
+    temp_1.setDaemon = True
+    temp_2.setDaemon = True
+    temp_3.setDaemon = True
+    temp_4.setDaemon = True
+    temp_5.setDaemon = True
+    temp_6.setDaemon = True
 
-    for i in range(3):
-        t = threading.Thread(target=trabajo)
-        threads.append(t)
-        t.start()
+    temp_1.start()
+    temp_2.start()
+    temp_3.start()
+    temp_4.start()
+    temp_5.start()
+    temp_6.start()
 
-    print(threads)
-    print_matrix(x_size, y_size, matrix, letter)
+    temp_1.join()
+    temp_2.join()
+    temp_3.join()
+    temp_4.join()
+    temp_5.join()
+    temp_6.join()
 
-def elevada():
-    pass
-def matriz_simetrica():
-    pass
-def matriz_identidad():
-    pass
-def multiplicar_ab():
-    pass
-def restar_ab():
-    pass
-def dividir_ab():
-    pass
+    print(temp_1)
+    print(temp_2)
+    print(temp_3)
+    print(temp_4)
+    print(temp_5)
+    print(temp_6)
+
+    # temp_7 = temp_1 + temp_2 + temp_3 - temp_4 - temp_5 - temp_6
+    # return temp_7
+
+# def transpuesta(matrix_1):
+#     pass
+# def inversa(matrix_1):
+#     pass
+# def multiplicar(matrix_1):
+#     threads = list()
+#
+#     num = input("Multiplicar por: ")
+#     for x in range(x_size):
+#         for y in range(y_size):
+#             # matrix = threading.Thread(target=)
+#             print('['+str(matrix[x][y]), end=']\t')
+#
+#
+#     threads = list()
+#
+#     for i in range(3):
+#         t = threading.Thread(target=trabajo)
+#         threads.append(t)
+#         t.start()
+#
+#     print(threads)
+#     print_matrix(x_size, y_size, matrix, letter)
+#
+# def elevada(matrix_1):
+#     pass
+# def matriz_simetrica(matrix_1):
+#     pass
+# def matriz_identidad(matrix_1):
+#     pass
+# def multiplicar_ab():
+#     pass
+# def restar_ab():
+#     pass
+# def dividir_ab():
+#     pass
 
 
 #SWITCH
