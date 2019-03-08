32a5ba7a0b09352ef409e87436bec069144f2b3e
Cristhian Urrego Salazar <infoafuro@gmail.com>
2019-03-08 10:54:19 -0500

    add POO

--- 01 chat/cliente.py
+++ 01 chat/cliente.py
@@ -1,31 +1,31 @@
 import socket, string, sys, os, time
-
 clear = lambda: os.system('cls')
 
-def principal():
-    mi_socket = socket.socket()
-    mi_socket.connect(('127.0.0.1',9090))
+class Cliente():
 
-    global nombre
-    nombre = input("[INGRESE SU NOMBRE]: ")
-    mi_socket.send(nombre.encode())
-    recibido=mi_socket.recv(1024)
-    print(recibido.decode())
-    time.sleep(.500)
+    def __init__(self):
+        self.mi_socket = socket.socket()
+        self.mi_socket.connect(('127.0.0.1',9090))
 
-    clear()
-
-    while True:
-        mensaje = input("["+nombre+"]: ")
-        mi_socket.send(mensaje.encode())
-        if(mensaje == 'cerrar'):
-            break
-        recibido=mi_socket.recv(1024)
+        global nombre
+        nombre = input("[INGRESE SU NOMBRE]: ")
+        self.mi_socket.send(nombre.encode())
+        recibido=self.mi_socket.recv(1024)
         print(recibido.decode())
+        time.sleep(.500)
 
-    print("adios")
-    mi_socket.close()
+        clear()
+
+        while True:
+            mensaje = input("["+nombre+"]: ")
+            self.mi_socket.send(mensaje.encode())
+            if(mensaje == 'cerrar'):
+                break
+            recibido=self.mi_socket.recv(1024)
+            print(recibido.decode())
+
+        print("adios")
+        self.mi_socket.close()
 
 
-if (__name__ == '__main__'):
-    principal()
+cliente = Cliente()
--- 01 chat/servidor.py
+++ 01 chat/servidor.py
@@ -1,24 +1,24 @@
 import socket
 
-def principal():
-    mi_socket = socket.socket()
-    mi_socket.bind(("",9090))
-    mi_socket.listen(1)
-    conexion, direccion = mi_socket.accept()
+class Servidor():
+    def __init__(self):
+        self.mi_socket = socket.socket()
+        self.mi_socket.bind(("",9090))
+        self.mi_socket.listen(1)
+        conexion, direccion = self.mi_socket.accept()
 
-    global nombre
-    nombre = conexion.recv(1024)
-    conexion.send("{{nombre aceptado}}".encode())
+        global nombre
+        nombre = conexion.recv(1024)
+        conexion.send("{{nombre aceptado}}".encode())
 
-    while True:
-        mensaje = conexion.recv(1024)
-        print("["+nombre.decode()+"]: "+mensaje.decode())
-        conexion.send("{{recibido}}".encode())
-        if(mensaje.decode()== 'cerrar'):
-            break
-    print("close")
-    conexion.close()
-    mi_socket.close()
+        while True:
+            mensaje = conexion.recv(1024)
+            print("["+nombre.decode()+"]: "+mensaje.decode())
+            conexion.send("{{recibido}}".encode())
+            if(mensaje.decode()== 'cerrar'):
+                break
+        print("close")
+        conexion.close()
+        self.mi_socket.close()
 
-if __name__ == '__main__':
-    principal()
+servidor = Servidor()
