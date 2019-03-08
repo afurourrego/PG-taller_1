1d4a967454977750727c23a7c92cd88cd1d28f42
Cristhian Urrego Salazar <infoafuro@gmail.com>
2019-03-08 15:35:54 -0500

    diccionario POO

--- 04 sockets/c diccionario/servidor.py
+++ 04 sockets/c diccionario/servidor.py
@@ -1,22 +1,22 @@
 import socket, diccionario
 
-def principal():
-    mi_socket = socket.socket()
-    mi_socket.bind(("",9090))
-    mi_socket.listen(1)
-    conexion, direccion = mi_socket.accept()
+class Servidor():
+    def __init__(self):
+        self.mi_socket = socket.socket()
+        self.mi_socket.bind(("",9090))
+        self.mi_socket.listen(1)
+        conexion, direccion = self.mi_socket.accept()
 
-    while True:
-        mensaje = conexion.recv(1024)
-        print("[]: "+mensaje.decode())
+        while True:
+            mensaje = conexion.recv(1024)
+            print("[]: "+mensaje.decode())
 
-        mensaje = diccionario.traducir(mensaje.decode())
+            mensaje = diccionario.traducir(mensaje.decode())
 
-        conexion.send("[BINARIO]: ".encode() + str(mensaje).encode())
+            conexion.send("[BINARIO]: ".encode() + str(mensaje).encode())
 
-    print("close")
-    conexion.close()
-    mi_socket.close()
+        print("close")
+        conexion.close()
+        self.mi_socket.close()
 
-if __name__ == '__main__':
-    principal()
+servidor = Servidor()
